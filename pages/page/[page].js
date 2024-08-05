87c45a078c0b2dfc9065364a3215a35cc7cf85e5
hugokoo <idong1111@gmail.com>
2024-08-05 17:35:26 +0800

    fix summary

--- pages/page/[page].js
+++ pages/page/[page].js
@@ -16,6 +16,40 @@ const Page = ({ postsToShow, page, showNext }) => {
     </Container>
   )
 }
+// export async function getStaticProps(context) {
+//   const { page } = context.params // Get Current Page No.
+//   const posts = await getAllPosts({ includePages: false })
+//   let postsToShow = posts.slice(
+//     config.postsPerPage * (page - 1),
+//     config.postsPerPage * page
+//   )
+//   const totalPosts = posts.length
+//   const showNext = page * config.postsPerPage < totalPosts
+//   // Setting up previews.
+//   await Promise.all(postsToShow.map(async (post, index) => {
+//     // Only run without summary
+//     if (!post.summary) {
+//       const blockMap = await getPostBlocks(post.id);
+//       const content = ReactDOMServer.renderToString(
+//         <ConfigProvider value={clientConfig}>
+//           <NotionRenderer recordMap={blockMap} />
+//         </ConfigProvider>
+//       )
+//       const regexExp = /(<).*?(>)/g
+//       const regexExp2 = /^(.*?)summary/g
+//       postsToShow[index].preview = content.replace(regexExp, '').replace(regexExp2, '').substring(0, 70) + '...';
+//     }
+//   }));
+//   return {
+//     props: {
+//       page, // current page is 1
+//       postsToShow,
+//       showNext
+//     },
+//     revalidate: 1
+//   }
+// }
+
 export async function getStaticProps(context) {
   const { page } = context.params // Get Current Page No.
   const posts = await getAllPosts({ includePages: false })
@@ -42,7 +76,7 @@ export async function getStaticProps(context) {
   }));
   return {
     props: {
-      page, // current page is 1
+      page, // Current Page
       postsToShow,
       showNext
     },
@@ -50,26 +84,7 @@ export async function getStaticProps(context) {
   }
 }
 
-// export async function getStaticProps (context) {
-//   const { page } = context.params // Get Current Page No.
-//   const posts = await getAllPosts({ includePages: false })
-//   const postsToShow = posts.slice(
-//     config.postsPerPage * (page - 1),
-//     config.postsPerPage * page
-//   )
-//   const totalPosts = posts.length
-//   const showNext = page * config.postsPerPage < totalPosts
-//   return {
-//     props: {
-//       page, // Current Page
-//       postsToShow,
-//       showNext
-//     },
-//     revalidate: 1
-//   }
-// }
-
-export async function getStaticPaths () {
+export async function getStaticPaths() {
   const posts = await getAllPosts({ includePages: false })
   const totalPosts = posts.length
   const totalPages = Math.ceil(totalPosts / config.postsPerPage)
